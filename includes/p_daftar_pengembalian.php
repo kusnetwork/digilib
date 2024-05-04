fbdeaa4dc63df9dce4e6606afb2ef358fca825c3
kusnetwork <nakusdianta.work@gmail.com>
2024-05-05 00:18:10 +0700

    Update p_daftar_pengembalian.php
    Slide 16

--- includes/p_daftar_pengembalian.php
+++ includes/p_daftar_pengembalian.php
@@ -157,16 +157,16 @@
 				  <?php
 					$db=new MySQL();
 					$db->connect();
-					if($data[$i][4] == "0000-00-00 00:00:00")
+					if($data[$i][4] == "0000-00-00 00:00:00") 
+					// Artinya belum kembali
+						//datediff itu selisih hari dari tanggal pinjam sampai kembali 
 						$db->execute("SELECT datediff(now(), tglPinjam) 
 							FROM `tb_pinjam` 
-							WHERE datediff(now(), tglPinjam) < 30
-							AND kodePinjam = ".$data[$i][0]." "); 
+							WHERE kodePinjam = ".$data[$i][0]." "); 
 					else
 						$db->execute("SELECT datediff(tglKembali, tglPinjam) 
 							FROM `tb_pinjam` 
-							WHERE datediff(tglKembali, tglPinjam) < 30
-							AND kodePinjam = ".$data[$i][0]." ");
+							WHERE kodePinjam = ".$data[$i][0]." ");
 					$dnd=$db->get_dataset();
 					if($dnd[0][0] >= 7) {
 						$denda = $dnd[0][0] * 500;
